d9c098cd4374a154ef4320e6b04a43d049d3e2bf
Hiago Gimenez <hiagogimenezvieira888@gmail.com>
2025-01-06 20:56:17 -0300

    fdgtfhuh

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'dotenv/config'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -7,6 +8,7 @@ const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 //Pegar
 app.get('/usuarios', async (req, res) => {
